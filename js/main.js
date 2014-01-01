bf7bc3c5c299111de6e2d8174f167d971516e604
Vincent Nalupta <vincent.nalupta@jcrew.com>
2014-01-01 16:48:07 -0500

    added sticky nav

--- js/main.js
+++ js/main.js
@@ -1,1 +1,28 @@
+var MyApp = (function(){
+  // global
+  var stickyNav = false;
+  var $window = $(window);
 
+
+  var watchScroll = function(){
+    $window.scroll(function(){
+      var $navbar = $('#navbar');
+      if( stickyNav === false && $window.scrollTop() >= 505){
+        $navbar.addClass('stick');
+        stickyNav = true;
+        console.log('STICK!');
+      } else if (stickyNav === true && $window.scrollTop() < 505) {
+        $navbar.removeClass('stick');
+        stickyNav = false;
+      }
+    });
+  };
+
+  var init = function(){
+    watchScroll();
+  };
+
+  return {
+    init : init
+  }
+})();
