bba3adc0fbedfb439a214869a5533efe3d127b27
Vincent Nalupta <vincent.nalupta@jcrew.com>
2014-01-01 17:33:51 -0500

    styling

--- js/main.js
+++ js/main.js
@@ -1,28 +1,46 @@
 var MyApp = (function(){
-  // global
-  var stickyNav = false;
-  var $window = $(window);
+	// global vars
+	var $window = $(window),
+	stickyNav = false;
+
+	//// PRIVATE FUNCS
 
 
-  var watchScroll = function(){
-    $window.scroll(function(){
-      var $navbar = $('#navbar');
-      if( stickyNav === false && $window.scrollTop() >= 505){
-        $navbar.addClass('stick');
-        stickyNav = true;
-        console.log('STICK!');
-      } else if (stickyNav === true && $window.scrollTop() < 505) {
-        $navbar.removeClass('stick');
-        stickyNav = false;
-      }
-    });
-  };
+	//// PUBLIC FUNCTIONS
+	// window scrolling events
+	var watchScroll = function(){
+		$window.scroll(function(){
+			var $navbar = $('#navbar'),
+			$body = $('body');
+			if( stickyNav === false && $window.scrollTop() >= 505){
+				$navbar.addClass('stick');
+				stickyNav = true;
+				$body.changeBackground('#E9E9E9');
+				$navbar.changeBackground('#51A0CD').addClass('about');
+			}
+			else if (stickyNav === true && $window.scrollTop() < 505) {
+				$navbar.removeClass('stick');
+				stickyNav = false;
+				$body.changeBackground('#24345A');
+				$navbar.changeBackground('#BE342A').removeClass('about');
+			}
+		});
+	};
+	var init = function(){
+		watchScroll();
+	};
 
-  var init = function(){
-    watchScroll();
-  };
-
-  return {
-    init : init
-  }
+	return {
+		init : init
+	}
 })();
+
+// jQuery helper plugins
+(function($){
+	$.fn.changeBackground = function(hex){
+		var fadeDuration = 200;
+		return this.animate({
+			backgroundColor : hex
+		}, fadeDuration );
+	}
+})(jQuery);
