afdeeaba5b661ec08576705b90a489e5337e7a3b
Vincent Nalupta <vincent.nalupta@jcrew.com>
2014-01-01 22:38:12 -0500

    added work section and nav clicks

--- js/main.js
+++ js/main.js
@@ -3,31 +3,60 @@ var MyApp = (function(){
 	var $window = $(window),
 	stickyNav = false;
 
-	//// PRIVATE FUNCS
+	// nav clicks
+	var bindNavEvents = function() {
+		$('#navbar').on('click','a',function(e){
+			var $body = $('body');
+			e.preventDefault();
+			e.stopPropagation();
 
-
-	//// PUBLIC FUNCTIONS
+			switch ($(this).attr('href')){
+				case "#about" : $body.animate({ scrollTop : '505px' }, 350 );
+								break;
+				case "#work"  : $body.animate({ scrollTop : '1140px' }, 350 );
+								break;
+				case "#blog"  : console.log('test');
+								break;
+				default 	  : break;
+			}
+		})
+	};
+	// dynamically set padding of nav to home slide
+	var setNavPadding = function(){
+		var offset = $('#home').offset().left;
+		$('#navbar').find('ul').css('paddingLeft',offset);
+	};
+	// on resize browser
+	var watchResize = function(){
+		$window.on('resize',function(){
+			setNavPadding();
+		});
+	};
 	// window scrolling events
 	var watchScroll = function(){
 		$window.scroll(function(){
+			console.log($window.scrollTop());
 			var $navbar = $('#navbar'),
 			$body = $('body');
 			if( stickyNav === false && $window.scrollTop() >= 505){
 				$navbar.addClass('stick');
 				stickyNav = true;
 				$body.changeBackground('#E9E9E9');
-				$navbar.changeBackground('#51A0CD').addClass('about');
+				$navbar.find('ul').changeBackground('#51A0CD').addClass('about');
 			}
 			else if (stickyNav === true && $window.scrollTop() < 505) {
 				$navbar.removeClass('stick');
 				stickyNav = false;
 				$body.changeBackground('#24345A');
-				$navbar.changeBackground('#BE342A').removeClass('about');
+				$navbar.find('ul').changeBackground('#BE342A').removeClass('about');
 			}
 		});
 	};
 	var init = function(){
 		watchScroll();
+		watchResize();
+		setNavPadding();
+		bindNavEvents();
 	};
 
 	return {
