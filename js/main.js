bb6f7cb24fe695eaad3cb74521f0c91d4d72d7a8
Vincent Nalupta <vincent.nalupta@jcrew.com>
2014-01-03 10:01:06 -0500

    fixed bug where screen elements not set on page refresh

--- js/main.js
+++ js/main.js
@@ -1,17 +1,32 @@
 var MyApp = (function(){
 	// global vars
 	var $window = $(window),
-    isStickyNav = false,
-    isDefaultBg = true,
-    isBottom = false,
-    isTop = true;
+        $navbar = $('#navbar'),
+        $navbarUL = $('#navbarUL'),
+        $body = $('body'),
+        $scrollTop = 0,
+        isStickyNav = false,
+        isDefaultBg = true,
+        isBottom = false,
+        isTop = true;
 
 	// dynamically set padding of nav to home slide
 	var setNavPadding = function(){
 		var offset = $('.sidebar').offset().left;
 		$('#navbar').find('ul').css('paddingLeft',offset);
 	};
-
+    var setBackgroundColor = function(){
+        setTimeout(function(){
+            _preparePage($('body').scrollTop());
+        }, 50);
+    };
+    // click events for portfolio
+    var bindSidebarEvents = function(){
+        $('.sidebar').on('click','a',function(){
+            $('.sidebar li a').removeClass('selected');
+            $(this).addClass('selected');
+        })
+    }
 	// nav clicks
 	var bindNavEvents = function() {
 		$('#navbar').on('click','a',function(e){
@@ -38,46 +53,64 @@ var MyApp = (function(){
 	};
 	// window scrolling events
 	var watchScroll = function(){
-		var $navbar = $('#navbar'),
-            $navbarUL = $('#navbarUL'),
-            $scrollTop = 0,
-            $body = $('body');
         $window.scroll(function(){
-            $scrollTop = $window.scrollTop();
-            if($scrollTop > 1150){
-                if(isStickyNav){
-                    $navbar.removeClass('stick').addClass('bottom');
-                    isStickyNav = false;
-                }
-            } else if ( $scrollTop >= 505 ){
-                if(!isStickyNav){
-                    $navbar.addClass('stick').removeClass('bottom');
-                    isStickyNav = true;
-                }
-            } else if ( $scrollTop >= 250 ){
-                if( isStickyNav ){
-                    $navbar.removeClass('stick');
-                    isStickyNav = false;
-                }
-                if( isDefaultBg ){
-                    $body.changeBackground('#E9E9E9');
-                    $navbarUL.changeBackground('#51A0CD');
-                    isDefaultBg = false;
-                }
-            } else if ( $scrollTop < 250 ){
-                if( !isDefaultBg ){
-                    $body.changeBackground('#24345A');
-                    $navbarUL.changeBackground('#BE342A');
-                    isDefaultBg = true;
-                }
-            }
+            setScrollItems($window.scrollTop());
         });
     };
+    var preparePage = function(scrollTop){
+        if(scrollTop > 1150){
+            console.log('isStickyNav: ' + isStickyNav)
+            if(isStickyNav){
+                $navbar.removeClass('stick').addClass('bottom');
+                isStickyNav = false;
+            } else  {
+                $navbar.addClass('bottom');
+            }
+            if( isDefaultBg ){
+                $body.changeBackground('#E9E9E9');
+                $navbarUL.changeBackground('#51A0CD');
+                isDefaultBg = false;
+            }
+        } else if ( scrollTop >= 505 ){
+            if(!isStickyNav){
+                $navbar.addClass('stick').removeClass('bottom');
+                isStickyNav = true;
+            }
+            if( isDefaultBg ){
+                $body.changeBackground('#E9E9E9');
+                $navbarUL.changeBackground('#51A0CD');
+                isDefaultBg = false;
+            }
+        } else if ( scrollTop >= 250 ){
+            if( isStickyNav ){
+                $navbar.removeClass('stick');
+                isStickyNav = false;
+            }
+            if( isDefaultBg ){
+                $body.changeBackground('#E9E9E9');
+                $navbarUL.changeBackground('#51A0CD');
+                isDefaultBg = false;
+            }
+        } else if ( scrollTop < 250 ){
+            if( !isDefaultBg ){
+                $body.changeBackground('#24345A');
+                $navbarUL.changeBackground('#BE342A');
+                isDefaultBg = true;
+            }
+        }
+    };
+    var setScrollItems = function(){
+        setTimeout(function(){
+            preparePage($window.scrollTop());
+        }, 100 );
+    };
     var init = function(){
+        setScrollItems();
+        setNavPadding();
         watchScroll();
         watchResize();
-        setNavPadding();
         bindNavEvents();
+        bindSidebarEvents();
     };
 
     return {
