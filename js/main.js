c7d75aa693794cff94870980f3f89185bc94c348
Vincent Nalupta <vincent.nalupta@jcrew.com>
2014-01-02 22:06:21 -0500

    reworked scroll logic yet again and added top and bottom docking

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
 var MyApp = (function(){
 	// global vars
 	var $window = $(window),
-		isStickyNav = false,
-		isDefaultBg = true,
-		isBottom = false;
+    isStickyNav = false,
+    isDefaultBg = true,
+    isBottom = false,
+    isTop = true;
 
 	// dynamically set padding of nav to home slide
 	var setNavPadding = function(){
@@ -20,14 +21,14 @@ var MyApp = (function(){
 
 			switch ($(this).attr('href')){
 				case "#about" : $body.animate({ scrollTop : '505px' }, 350 );
-								break;
-				case "#work"  : $body.animate({ scrollTop : '1140px' }, 350 );
-								break;
-				case "#blog"  : console.log('test');
-								break;
-				default 	  : break;
-			}
-		})
+                break;
+                case "#work"  : $body.animate({ scrollTop : '1140px' }, 350 );
+                break;
+                case "#blog"  : console.log('test');
+                break;
+                default 	  : break;
+            }
+        })
 	};
 	// on resize browser
 	var watchResize = function(){
@@ -38,43 +39,50 @@ var MyApp = (function(){
 	// window scrolling events
 	var watchScroll = function(){
 		var $navbar = $('#navbar'),
-			$navbarUL = $('#navbarUL'),
-			$scrollTop = 0,
-			$body = $('body');
-		$window.scroll(function(){
-			$scrollTop = $window.scrollTop();
-			// change background color
-			if(isDefaultBg === true && $scrollTop >= 250){
-                $body.changeBackground('#E9E9E9');
-                $navbarUL.changeBackground('#51A0CD');
-                isDefaultBg = false;
-            } else if(isDefaultBg === false && $scrollTop < 250){
-                $body.changeBackground('#24345A');
-                $navbarUL.changeBackground('#BE342A');
-                isDefaultBg = true;
+            $navbarUL = $('#navbarUL'),
+            $scrollTop = 0,
+            $body = $('body');
+        $window.scroll(function(){
+            $scrollTop = $window.scrollTop();
+            if($scrollTop > 1150){
+                if(isStickyNav){
+                    $navbar.removeClass('stick').addClass('bottom');
+                    isStickyNav = false;
+                }
+            } else if ( $scrollTop >= 505 ){
+                if(!isStickyNav){
+                    $navbar.addClass('stick').removeClass('bottom');
+                    isStickyNav = true;
+                }
+            } else if ( $scrollTop >= 250 ){
+                if( isStickyNav ){
+                    $navbar.removeClass('stick');
+                    isStickyNav = false;
+                }
+                if( isDefaultBg ){
+                    $body.changeBackground('#E9E9E9');
+                    $navbarUL.changeBackground('#51A0CD');
+                    isDefaultBg = false;
+                }
+            } else if ( $scrollTop < 250 ){
+                if( !isDefaultBg ){
+                    $body.changeBackground('#24345A');
+                    $navbarUL.changeBackground('#BE342A');
+                    isDefaultBg = true;
+                }
             }
-            // sticky nav
-            if( isStickyNav === false && $scrollTop >= 505 ){
-                $navbar.addClass('stick');
-                isStickyNav = true;
-            }
-            else if (isStickyNav === true && $scrollTop < 505 ){ // check at 1100
-                $navbar.removeClass('stick');
-                isStickyNav = false;
-            }
+        });
+    };
+    var init = function(){
+        watchScroll();
+        watchResize();
+        setNavPadding();
+        bindNavEvents();
+    };
 
-		});
-	};
-	var init = function(){
-		watchScroll();
-		watchResize();
-		setNavPadding();
-		bindNavEvents();
-	};
-
-	return {
-		init : init
-	}
+    return {
+      init : init
+    }
 })();
 
 // jQuery helper plugins
